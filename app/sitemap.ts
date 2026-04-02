124081eea5bdda4403c2056c1712e05ee7961106
athersaud-smart-tools <athersaud@gmail.com>
2026-04-02 21:03:51 +0500

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://smart-tools-eta.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://smart-tools-eta.vercel.app/image/resize",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://smart-tools-eta.vercel.app/image/compress",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://smart-tools-eta.vercel.app/typing-test",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://smart-tools-eta.vercel.app/pdf/merge",
+      lastModified: new Date(),
+    },
+  ];
+}
