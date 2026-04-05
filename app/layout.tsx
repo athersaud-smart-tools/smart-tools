428dae48ae79ac8c14e9f6557c0906d66dcb1063
athersaud-smart-tools <athersaud@gmail.com>
2026-04-05 11:45:42 +0500

    added adsense code

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8430178179260712"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
