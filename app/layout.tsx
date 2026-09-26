ce11eb5f03f954c8416563c5f2cf6aac0af8d9e4
athersaud-smart-tools <athersaud@gmail.com>
2026-09-26 09:49:58 +0300

    Add Microsoft Clarity tracking to site head

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,6 +84,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8430178179260712"
           crossOrigin="anonymous"
         />
+
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "y06pqwd7q7");
+            `,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
